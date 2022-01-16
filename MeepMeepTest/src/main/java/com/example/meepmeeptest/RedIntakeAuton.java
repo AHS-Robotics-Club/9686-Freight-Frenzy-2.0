c60b4e0f189ce83fde17a5da729da97c254385b9
Vignesh SK <vigsk17@gmail.com>
2022-01-16 16:00:06 -0500

    Added other trajectories to MeepMeep for visuals

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/RedIntakeAuton.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/RedIntakeAuton.java
@@ -5,6 +5,10 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.ColorScheme;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -39,10 +43,32 @@ public class RedIntakeAuton {
                                 .build()
                 );
 
+        RoadRunnerBotEntity secondBot = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-12.0, 62.0, Math.toRadians(90.0)))
+                                .back(27.0)
+                                .turn(Math.toRadians(90.0))
+                                .forward(50.0)
+                                .build()
+                );
+
+        RoadRunnerBotEntity thirdBot = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueLight())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0.0, 62.0, Math.toRadians(0.0)))
+                            .forward(36.0)
+                            .build()
+                );
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(secondBot)
+                .addEntity(thirdBot)
                 .start();
     }
 }
